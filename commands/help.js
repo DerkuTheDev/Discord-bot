987bb4ab9ea8f6d81d753568a6ccdc6437d71f35
Joshua-M07 <64021298+Joshua-M07@users.noreply.github.com>
2020-05-02 13:08:41 +0100

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+module.exports = {
+  name: "help",
+  run: async (bot, message, args) => {
+    let command = message.content.toLowerCase().split(" ")[0];
+
+    const helpembed = new Discord.MessageEmbed().setColor("#7289DA")
+      .setDescription(`
+__**Commands List**__
+> \`corona\`, **\`invite\`**
+> \`oof\`, **\`slap\`**
+> \`warn\`, \`clear [amount]\`
+> \`meme\`, \` \``);
+    message.channel.send(helpembed);
